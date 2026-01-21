56c5031a29271b703b0009c8c9b696b3ded4e7d9
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2026-01-21 21:20:05 +0530

    Update error state in error middleware

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -2,7 +2,7 @@ const errorMiddleware = (err, req, res, next) => {
     console.error(err);
     res.status(err.statusCode || 500).json({
         success: false,
-        message: err.message || 'Internal Server Error'
+        message: err.message || 'Internal Server Error Or Failed'
     });
 }
 
